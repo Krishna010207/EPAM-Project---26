8d86bfa1f20e51155caf9077b6ad4139f0f28d12
Krishna010207 <priyakrishnakp1707@gmail.com>
2026-08-07 10:52:44 +0530

    second commit

--- emergency_route_validation.java
+++ emergency_route_validation.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class emergency_route_validation7 {
+public class emergency_route_validation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,13 +8,13 @@ public class emergency_route_validation7 {
         int M = sc.nextInt();
         int D = sc.nextInt();
 
-        // Adjacency list
+        
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             adj.add(new ArrayList<>());
         }
 
-        // Read edges
+        
         for (int i = 0; i < M; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
@@ -22,13 +22,13 @@ public class emergency_route_validation7 {
             adj.get(v).add(u);
         }
 
-        // Distance array (-1 means unvisited)
+        
         int[] dist = new int[N + 1];
         Arrays.fill(dist, -1);
 
         Queue<Integer> queue = new LinkedList<>();
 
-        // BFS from city 1
+        
         queue.add(1);
         dist[1] = 0;
 
@@ -43,7 +43,7 @@ public class emergency_route_validation7 {
             }
         }
 
-        // Count cities with distance ≤ D
+        
         int count = 0;
         for (int i = 1; i <= N; i++) {
             if (dist[i] != -1 && dist[i] <= D) {
